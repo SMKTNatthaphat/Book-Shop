d924de28ab2a78d31edec59fa472b650af23a714
SiameseDog <siamesedogbook@gmail.com>
2016-06-20 13:36:17 +0700

    จากหน้าหลักไปหน้า2

--- app/src/main/java/neu/chaiyasittirak/natthaphat/bookshop/MainActivity.java
+++ app/src/main/java/neu/chaiyasittirak/natthaphat/bookshop/MainActivity.java
@@ -1,7 +1,9 @@
 package neu.chaiyasittirak.natthaphat.bookshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }// Main Methog
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
-}
+
+
+} // Main Class นี้คือ คลาสหลัก
